2f9ac06ff6bfd76ae13fd3f7e4e900845cc42ffe
summerghost <frequn00@gmail.com>
2020-04-27 00:47:29 +0700

    uPDATE tO MaiN

--- Tubes Files/main.cpp
+++ Tubes Files/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "listRelasi.h"
 
+using namespace std;
+
+int main() {
+	do {
+		cout << "======================================" << endl;
+		cout << "===                                ===" << endl;
+		cout << "===    MENU ADMINISTRASI WARTEG    ===" << endl;
+		cout << "===            v0.1a               ===" << endl;
+		cout << "===                                ===" << endl;
+		cout << "======================================" << endl;
+	}
+	return 0;
+}
