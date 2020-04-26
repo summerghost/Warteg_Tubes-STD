c3d86f7c37f144f1f94696c20c2199a57aa35147
msulthon <muhammadsulthon82@gmail.com>
2020-04-26 22:38:22 +0700

    parent 2

--- Tubes Files/listParrent.h
+++ Tubes Files/listParrent.h
@@ -27,3 +27,15 @@ address_child CreateElmChild(infotypeParent in);
 void insertFirstChild(List_Child &L, address_child P);
 void insertAfterChild(List_Child &L, address_child Prec, address_child P);
 void insertLastChild(List_Child &L, address_child P);
+
+
+void insertSortedParent(List_parent &L, infotypeParent X);
+void deleteFirstParent(List_Parent &L, address_parent &P);
+void deleteAfterParent(List_Parent &L, address_parent Prec, address_parent &P);
+void deleteLastParent(List_Parent &L, address_parent &P);
+void deleteSpesificParent(List_Parent &L, string X);
+address_child findElmParent(List_Parent &L, string X);
+void printInfoParent(List_Parent L);
+
+void inputDataTahanan()Warteg(List_Parent L,infotypeParent &in);
+
