6c547a3cc035d6db2b97456c7a29820e7cb05cc6
msulthon <muhammadsulthon82@gmail.com>
2020-04-26 22:18:51 +0700

    Update listParrent.h

--- Tubes Files/listParrent.h
+++ Tubes Files/listParrent.h
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
 
+using namespace std;
+
+struct infotypeParent {
+    string Warteg;
+    string IDWarteg;
+    int kapasitas;
+};
+
+typedef struct parent *address_parent;
+
+struct parent {
+    infotypeParent info;
+    address_parent next;
+};
+
+struct List_Parent {
+    address_parent first;
+    address_parentlast;
+};
